202714c4847ac988ae4b97ef50ff1e4059053ba0
priyanshi012 <priyanshisaini1703@gmail.com>
2026-02-03 18:36:22 +0000

    let,var,const

---
+++ Basics/variable.js
@@ -0,0 +1,14 @@
+console.log("hii this is priyanshi and today is the first day of learning js");
+//we cannot change the value declare using const
+const accoundId= 1443
+let accountEmail ="pari@google.com"
+var accoundPass ="1234"
+accountCity ="jaipur";
+let accountState;
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.log(accoundId);
+console.table([accoundId,accountEmail,accoundPass,accountState]);
