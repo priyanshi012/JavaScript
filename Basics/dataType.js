cf83929f7193f9995c3d842a1a7ef13c870096db
priyanshi012 <priyanshisaini1703@gmail.com>
2026-02-05 19:15:23 +0000

    dataTypes

---
+++ Basics/dataType.js
@@ -0,0 +1,20 @@
+"use strict" //treat all js code as newer version
+// alert("hello"); //we are using nodejs not browser
+ let name="priyanshi"
+ let age = 20
+ let isLoggedIn = false
+ //number => 2 to power 53
+  //bigint //big value
+  //string =>""
+  //boolean => true/false
+  //null => standlone value
+  //undefined =>
+    //symbol => to find uniqueness
+
+
+  //object
+  console.log(typeof"Priya");
+  console.log(typeof age);
+  console.log(typeof null); //object
+  console.log(typeof undefined); //undefined
+  
