4c16c897e86e71cbb10cc22ac19c15a16cd55ac4
priyanshi012 <priyanshisaini1703@gmail.com>
2026-02-06 20:01:43 +0000

    conversion

---
+++ Basics/conversion.js
@@ -0,0 +1,32 @@
+// let score="33";
+// console.log(typeof score);
+// let valInNumber = Number(score);
+// console.log(typeof valInNumber);
+//"33"=> if we covert a string then its easily convert it into and Integer
+//"33abc" => NaN
+// true =>1; false=>0; 
+
+//try some more
+// let isLoggedIn =1
+// let booleansIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleansIsLoggedIn);
+
+//1=> true;0=> false
+//""=> false
+//"hitesh"=> true
+
+
+//*****operations******* */
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+
+console.log(+true);
+console.log(+"");
+
+
