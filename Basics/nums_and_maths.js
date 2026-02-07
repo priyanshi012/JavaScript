904ec2b037dea998f45f7e4e87d5491a0f9fe3ce
priyanshi012 <priyanshisaini1703@gmail.com>
2026-02-07 12:55:14 +0000

    nums_and_maths

---
+++ Basics/nums_and_maths.js
@@ -0,0 +1,27 @@
+// const score = 20;
+
+//new object
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// //used for decimal
+// console.log(balance.toFixed(2));
+// console.log(score);
+
+// const otherNumber = 123.945
+// console.log(otherNumber.toPrecision(3));
+
+//maths//
+// maths lin js ke sath bydefault ati h
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+
+console.log(Math.min(2,3,4,5));
+// Math.random value always lie btw 0-1 
+// console.log(Math.random()*10 + 1); 
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1)+min))
