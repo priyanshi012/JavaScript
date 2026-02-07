86474f58d91331f4fd4c90c6c22e588f2353dee3
priyanshi012 <priyanshisaini1703@gmail.com>
2026-02-07 13:57:04 +0000

    Array

---
+++ Basics_02/Arrays.js
@@ -0,0 +1,55 @@
+//arrays
+const myArr = [0,1,2,3,4]
+// const arr =["priya","priyanshi"]
+// const arr2 = new Array(1,2,3,4)
+// console.log(myArr[1]);
+
+//methods of array
+// myArr.push(6) //add value to the array
+// console.log(myArr);
+// myArr.pop() //reove last value 
+// console.log(myArr);
+
+//unshift(value) add value at starting of the array
+//shift(value) remove value from start
+
+//includes used to search an element => return boolean value
+// indexOf => return -1 value
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+
+//slice and splice
+// console.log("a",myArr);
+// const arr1 = myArr.slice(1,3)
+// console.log(arr1);
+// console.log("B",myArr);
+// //splice
+// const arr = myArr.splice(1,3)
+// console.log("C",myArr);
+// console.log(arr);
+
+
+//concate two arrays
+const arr1 = ["mango","apple"]
+const arr2 =["pari","rahul"]
+
+// const merge = arr1.concat(arr2);
+// console.log(merge);
+
+//more easy way => spread
+// const all = [...arr1,...arr2];
+// console.log(all);
+
+//flat is used when array inside another array to make it simple
+
+// console.log(Array.isArray("priya"));
+// console.log(Array.from("hitesh"));
+// console.log(Array.from({name:"hitesh"}));
+
+// let score =100;
+// let score2 =200;
+// let score3 =300;
+
+// console.log(Array.of(score,score2,score3));
