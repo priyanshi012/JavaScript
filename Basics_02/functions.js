3a244c1926daddbf060f6fbae9309303e707447d
priyanshi012 <priyanshisaini1703@gmail.com>
2026-02-16 17:37:34 +0000

    functions

--- Basics_02/functions.js
+++ Basics_02/functions.js
@@ -1,14 +1,36 @@
-function sayMyName(){
-    console.log("priyanshi");
+// function sayMyName(){
+//     console.log("priyanshi");
     
-}
-sayMyName()
+// }
+// sayMyName()
 
-function loginUser(username){
-    if(username=== undefined){
-        console.log("please enter a username");
-        return
-    }
-  return `${username} just Logged in`
-}
-console.log(loginUser())
+// function loginUser(username){
+//     if(username=== undefined){
+//         console.log("please enter a username");
+//         return
+//     }
+//   return `${username} just Logged in`
+// }
+// console.log(loginUser())
+//...spread operator
+// function calculateCartPrice(...num){
+//     return num;
+// }
+// console.log(calculateCartPrice(200,300,400));
+
+//object ko function mei kaise pass kre
+// const user ={
+//     username:"priyanshi",
+//     price:200
+// }
+// function handleObj(anyObj){
+//   console.log(`username is ${anyObj.username} price is ${anyObj.price}`);
+  
+// }
+// handleObj(user);
+
+// const arr = [200,300,400]
+// function returnSecondValue(getArray){
+//   return getArray[1]
+// }
+// console.log(returnSecondValue(arr));
