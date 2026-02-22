392b911910a5ee225deb7e7547c0da669f0716af
priyanshi012 <priyanshisaini1703@gmail.com>
2026-02-22 16:32:09 +0000

    control flow

---
+++ Basics_02/truthy.js
@@ -0,0 +1,16 @@
+//falsy values
+// false,0,-0,BigInt 0n,"",null,undefined,NaN
+
+// truthy values
+//"0",'false'," ",[],{},function{}
+
+//operator
+//Nullish Coalescing operator(??) Null undefined
+// let val1;
+
+// //  val1 = 5?? 10
+// // val1 = null ?? 10
+// val1 = undefined ?? 18
+//  console.log(val1);
+ 
+//terniary operator
