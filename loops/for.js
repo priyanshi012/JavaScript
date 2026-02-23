36a6f759edd94c94fde8049edccc9e4bf0b3f375
priyanshi012 <priyanshisaini1703@gmail.com>
2026-02-23 18:08:34 +0000

    for loop

--- loops/for.js
+++ loops/for.js
@@ -6,3 +6,18 @@
 // }
 // console.log();
 
+//for of loops
+// const arr =[1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num);    
+// }
+
+//map
+const map = new Map;
+map.set('IN',"india")
+map.set('FR',"france")
+console.log(map)
+for(const [key,value] of map){
+    console.log(key,':-',value);
+    
+}
